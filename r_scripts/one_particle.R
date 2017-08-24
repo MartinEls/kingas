9673ceafb7a070b70b096b4c4e280ad0825a39ee
Martin Elstner <MartinEls@users.noreply.github.com>
2017-08-24 10:07:04 +0200

    Define the MovePtcl function

--- r_scripts/one_particle.R
+++ r_scripts/one_particle.R
@@ -1,1 +1,26 @@
+# calculate new particle position
+MovePtcl <- function(step, size) {
+  # move particle in x and y direction
+  step$x <- step$x + (step$dir_x * step$speed)
+  step$y <- step$y + (step$dir_y * step$speed)
+  # check for wall collision and redirect
+  if(abs(step$x) >= size){
+    step$dir_x <- step$dir_x * (-1)
+    step$x <- size + sign(step$dir_x) * (step$x - size)
+  }
+  if(abs(step$y) >= size){
+    step$dir_y <- step$dir_y * (-1)
+    step$y <- size + sign(step$dir_y) * (step$y - size)
+  }
+  return(step)
+}
+
+# outer boundary box
+box_size <- 1000
+steps <- 50
+
+ptcl <- data.frame(x = 0, y = 300, dir_x = 0.71, dir_y = 0.71, speed = 100)
+ptcl <- MovePtcl(ptcl, box_size)
+
+
 ### use imagemagic script to generate an animation of the outputet plots.
